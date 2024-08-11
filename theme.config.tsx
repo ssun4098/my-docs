d97c5271e5c854b07bb53e753ee8e54d15d9569d
bebo0 <bebo007@daonplace.com>
2024-08-11 16:31:45 +0900

    스프링 시큐리티 구조 추가

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,16 +3,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  feedback: {
+    content: null
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  editLink: {
+    component: null
+  }
 }
 
 export default config
